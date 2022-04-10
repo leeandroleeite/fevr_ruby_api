436fd6fc95087e2b8e2e0a0e0e88f10906bdf704
Leandro Leite <leeandro.leeite@gmail.com>
2022-04-10 22:17:49 +0100

    fix: Improved player serializer to show notifications from each player

--- app/serializers/notification_serializer.rb
+++ app/serializers/notification_serializer.rb
@@ -1,3 +1,5 @@
 class NotificationSerializer < ActiveModel::Serializer
   attributes :id, :message
+
+  #belongs_to :player
 end
--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,3 +1,6 @@
 class PlayerSerializer < ActiveModel::Serializer
   attributes :id, :name, :number, :nationality, :age, :position
+
+  # Gets all information from players along with the notifications for each player
+  has_many :notifications
 end
