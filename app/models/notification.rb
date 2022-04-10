79ce95b69afaadf9814a9ce00efc3251096a4d07
Leandro Leite <leeandro.leeite@gmail.com>
2022-04-11 00:52:02 +0100

    fix: improved validations on models and added new specs files

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,5 @@
 class Notification < ApplicationRecord
   belongs_to :player
+  
+  validates :message, presence: true
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,8 @@
 class Player < ApplicationRecord
     has_many :notifications, dependent: :destroy
-    validates :name, presence: true
+
+    validates :name, :nationality, :age, presence: true
+    validates :number, :age, length: {in: 1..2}, numericality: {only_integer: true, greater_than: 0}
+    validates :number, uniqueness: true
 end
+
